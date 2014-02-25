cffdb2af2ed0975069be39cc026ff03df427270f
Justin Maier <just.maier@gmail.com>
2014-02-25 11:32:23 -0800

    v1

---
+++ README.md
@@ -0,0 +1,38 @@
+angular-csv-binding
+=========================
+
+Easily bind arrays of data to a textarea with CSV formating
+
+[**Check out a demo!**](http://justmaier.github.io/angular-csv-binding/)
+
+##Installation
+
+####Bower
+`bower install angular-csv-binding`
+
+####Manually
+`<script type="text/javascript" src="js/angular-csv-binding.js"></script>`
+
+##Usage
+
+1. Include the `angular-csv-binding.js` script provided by this component into your app
+2. add `csvBinding` as a module dependency to your app
+
+####Javascript
+```javascript
+angular.module('app', ['csvBinding'])
+.controller('demo', ['$scope', function($scope){
+	$scope.data = [{a:'test',b:'test2'}, {a:'test3',b:'test4'}];
+	$scope.delimiter = ',';
+	$scope.hasHeaders = true;
+}]);
+```
+
+```html
+<textarea class="form-control" rows="10" csv delimiter="delimiter" has-headers="hasHeaders" ng-model="data"></textarea> 
+<pre ng-bind="data | json"></pre>
+```
+
+##Options
+* `delimiter` the separator of the values
+* `hasHeaders` whether or not the data you are providing has headers for each column
---
+++ angular-csv-binding.js
@@ -0,0 +1,177 @@
+'use strict';
+
+angular.module('csvBinding', [])
+.directive('csv', [function(){
+	return {
+		require: 'ngModel',
+		restrict: 'CA',
+		scope: {
+			delimiter: '=',
+			hasHeaders: '='
+		},
+		link: function(scope, element, attr, ngModel){
+			scope.delimiter == scope.delimiter || ',';
+			scope.hasHeaders == angular.isDefined(scope.hasHeaders)? scope.hasHeaders : true;
+			var	headers = []; //To maintain order when adding headers
+
+			// This will parse a delimited string into an array of
+			// arrays. The default delimiter is the comma, but this
+			// can be overriden in the second argument.
+			function CSVToArray(strData){
+				var delimiter = scope.delimiter || ',',
+					hasHeaders = angular.isDefined(scope.hasHeaders)? scope.hasHeaders : true;
+
+				if(strData.indexOf(delimiter) == -1) return [];
+				headers = [];
+
+				// Create a regular expression to parse the CSV values.
+				var objPattern = new RegExp(
+					(
+						// Delimiters.
+						"(\\" + delimiter + "|\\r?\\n|\\r|^)" +
+
+						// Quoted fields.
+						"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+						// Standard fields.
+						"([^\"\\" + delimiter + "\\r\\n]*))"
+					),
+					"gi"
+					);
+
+
+				// Create an array to hold our data. Give the array
+				// a default empty first row.
+				var arrData = [[]];
+
+				// Create an array to hold our individual pattern
+				// matching groups.
+				var arrMatches = null;
+
+
+				// Keep looping over the regular expression matches
+				// until we can no longer find a match.
+				var column = 0;
+				while (arrMatches = objPattern.exec( strData )){
+
+					// Get the delimiter that was found.
+					var strMatchedDelimiter = arrMatches[ 1 ];
+
+					// Check to see if the given delimiter has a length
+					// (is not the start of string) and if it matches
+					// field delimiter. If id does not, then we know
+					// that this delimiter is a row delimiter.
+					if (strMatchedDelimiter.length && (strMatchedDelimiter != delimiter)){
+
+						// If this is the first row and we have headers
+						// capture the headers for future use
+						if(arrData.length == 1 && hasHeaders && !headers.length){
+							headers = arrData[0].slice(0);
+							arrData.splice(0,1);
+						}
+
+						// Since we have reached a new row of data,
+						// add an empty row to our data array.
+						arrData.push( headers.length?{}:[] );
+						column = 0;
+					}
+
+
+					// Now that we have our delimiter out of the way,
+					// let's check to see which kind of value we
+					// captured (quoted or unquoted).
+					if (arrMatches[ 2 ]){
+						// We found a quoted value. When we capture
+						// this value, unescape any double quotes.
+						var strMatchedValue = arrMatches[ 2 ].replace(
+							new RegExp( "\"\"", "g" ),
+							"\""
+							);
+
+					} else {
+						// We found a non-quoted value.
+						var strMatchedValue = arrMatches[ 3 ];
+
+					}
+
+
+					// Now that we have our value string, let's add
+					// it to the data array.
+					var currentRow = arrData[arrData.length - 1];
+					if(headers.length !== 0){
+						currentRow[headers[column]] = strMatchedValue;
+					}else{
+						currentRow.push( strMatchedValue );
+					}
+					column++;
+				}
+
+				// Return the parsed data.
+				return( arrData );
+			}
+
+			var parseCSV = function(value){
+				try{
+					var csv = CSVToArray(value);
+					return csv;
+				}catch(e){
+					console.log(e);
+					return value;
+				}
+			}
+
+			function ArrayToCSV(array){
+				var delimiter = scope.delimiter || ',',
+					hasHeaders = angular.isDefined(scope.hasHeaders)? scope.hasHeaders : true,
+					out = [];
+
+				//Get updated headers
+				if(hasHeaders){
+					var newHeaders = angular.copy(headers);
+					angular.forEach(array, function(item){
+						angular.forEach(item, function(value, key){
+							if(newHeaders.indexOf(key) == -1){
+								newHeaders.push(key);
+							}
+						});
+					});
+					out.push(newHeaders.join(delimiter));
+				}
+
+				//Prepare out string
+				angular.forEach(array, function(item){
+					var outLine = [];
+
+					if(hasHeaders){
+						angular.forEach(newHeaders, function(header){
+							outLine.push(item[header] || '');
+						});
+					}else{
+						angular.forEach(item, function(value){
+							outLine.push(value);
+						});
+					}
+					
+					out.push(outLine.join(delimiter));
+				});
+				return out.join('\n');
+			}
+
+			ngModel.$parsers.unshift(parseCSV);
+			ngModel.$formatters.push(ArrayToCSV);
+
+			var render = function(v){
+				var viewValue = ngModel.$modelValue;
+				if (viewValue == null || !viewValue.length) return;
+				for (var i in ngModel.$formatters) {
+					viewValue = ngModel.$formatters[i](viewValue);
+				}
+				ngModel.$viewValue = viewValue;
+				ngModel.$render();
+			}
+
+			scope.$watch('delimiter', render);
+			scope.$watch('hasHeaders', render);
+		}
+	}
+}]);
